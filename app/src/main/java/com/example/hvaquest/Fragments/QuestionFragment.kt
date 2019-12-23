85850648fd9e966097c6c73ad1d91ceec7f34ec6
xMISSJ <xmissj96@gmail.com>
2019-12-23 16:57:03 +0100

    Changes: Added QuestionFragment.kt and LocationFragment.kt with their xml files and created their layout. Will implement functionality soon.

--- app/src/main/java/com/example/hvaquest/Fragments/QuestionFragment.kt
+++ app/src/main/java/com/example/hvaquest/Fragments/QuestionFragment.kt
@@ -1,4 +1,4 @@
-package com.example.hvaquest
+package com.example.hvaquest.Fragments
 
 
 import android.os.Bundle
@@ -7,17 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import com.example.hvaquest.R
+
 /**
  * A simple [Fragment] subclass.
  */
-class StartFragment : Fragment() {
+class QuestionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        return inflater.inflate(R.layout.fragment_question, container, false)
     }
 
 
